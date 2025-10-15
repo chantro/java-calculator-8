ca062906be069fe9a817cabc7356588f8a203446
chantro <sophia2359@gmail.com>
2025-10-15 19:12:47 +0900

    feat(calculator):  ',' 또는 ':' 구분자와 숫자로만 구성된 문자열에서의 덧셈
    쉼표와 콜론을 기준으로 문자열을 파싱하여 각 파싱된 문자열을 int형 정수로 변환해 총합을 구합니다. 이때 예외 처리는 적용되지 않은 상태입니다.
    
    Examples:
    - "1,2:3" -> 6
    - "4:5" -> 9

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,22 @@
 package calculator;
+import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String input = Console.readLine();
+        List<String> delimiters = new ArrayList<>(List.of(",", ":"));
+
+        // 구분자를 기준으로 덧셈
+        String regex = "[" + String.join("", delimiters) + "]";
+        String[] nums = input.split(regex);
+        int sum = 0;
+        for(String str_num : nums){
+            int n = Integer.parseInt(str_num);
+            sum += n;
+        }
+        System.out.println("결과 : " + sum);
     }
 }
